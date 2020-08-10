8cfd2876598de1cc12b9d183a23d7341bd261bf5
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-08-10 12:18:56 -0300

    Added "minimal 3 characters" validation

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 const usersContainer = document.querySelector('ul');
 
-
-
 document.querySelector('button').addEventListener('click', () => {
-const query = document.querySelector('input').value;
+    usersContainer.innerHTML = ''
+    const query = document.querySelector('input').value;
 
     if(document.getElementById('search').value == ""){
         document.getElementById('search').focus();
         alert("Erro! Digite um nome a ser pesquisado!");
+    } else if (document.getElementById('search').value.length < 3){
+        document.getElementById('search').focus();
+        alert("Erro! Digite um nome de usuário com no mínimo 3 caracteres!");
     }
+
     else{
         axios.get(`https://api.github.com/search/users?q=${query}`)
         .then(res => {
             console.log(res)
             const users = res.data.items;
             for (let i = 0; i < users.length && i < 20; i++){
-                let li = document.createElement('li');
-                img = document.createElement('img');
-                var desc = document.createElement('p');
-                li.innerHTML = users[i].login;
+                const li = document.createElement('li');
+                const img = document.createElement('img');
+                const username = document.createElement('a');
+                username.innerHTML = users[i].login;
                 img.src = users[i].avatar_url;
-                desc.innerHTML = users[i].html_url;
+                username.href = users[i].html_url;
                 li.appendChild(img);
-                li.appendChild(desc);
+                li.appendChild(username);
                 usersContainer.appendChild(li);
             }
         })
