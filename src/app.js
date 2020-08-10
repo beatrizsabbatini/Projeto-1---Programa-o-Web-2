02ad0d9fc0370ee21b63ac7d553cda911d079e88
Juliana Yukari <julianayukari36@gmail.com>
2020-08-10 10:19:22 -0300

    Validação e uso de fotos

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 const usersContainer = document.querySelector('ul');
 
-document.querySelector('button').addEventListener('click', () => {
 
+
+document.querySelector('button').addEventListener('click', () => {
 const query = document.querySelector('input').value;
-    axios.get(`https://api.github.com/search/users?q=${query}`)
+
+    if(document.getElementById('search').value == ""){
+        document.getElementById('search').focus();
+        alert("Erro! Digite um nome a ser pesquisado!");
+    }
+    else{
+        axios.get(`https://api.github.com/search/users?q=${query}`)
         .then(res => {
             console.log(res)
             const users = res.data.items;
             for (let i = 0; i < users.length && i < 20; i++){
                 let li = document.createElement('li');
+                img = document.createElement('img');
+                var desc = document.createElement('p');
                 li.innerHTML = users[i].login;
+                img.src = users[i].avatar_url;
+                desc.innerHTML = users[i].html_url;
+                li.appendChild(img);
+                li.appendChild(desc);
                 usersContainer.appendChild(li);
             }
         })
+    }
 })
