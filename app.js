98577aa423304148f4469f08e36e18b0f44896cb
Beatriz Schwartz <beatriz.sabbatini@gmail.com>
2020-08-13 10:15:35 -0300

    Added validation without Alert

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const usersContainer = document.querySelector('ul');
+const errorContainer = document.querySelector('h4');
 
 document.querySelector('button').addEventListener('click', () => {
     usersContainer.innerHTML = ''
+    errorContainer.innerHTML = ''
     const query = document.querySelector('input').value;
 
     if(document.getElementById('search').value == ""){
         document.getElementById('search').focus();
-        alert("Erro! Digite um nome a ser pesquisado!");
+        let error = document.createElement('h4');
+        error.innerHTML = "Erro! Digite o nome a ser pesquisado!";
+        errorContainer.appendChild(error);
+
+
     } else if (document.getElementById('search').value.length < 3){
         document.getElementById('search').focus();
-        alert("Erro! Digite um nome de usuário com no mínimo 3 caracteres!");
+        let error = document.createElement('h4');
+        error.innerHTML = "Erro! Digite um nome de usuário com no mínimo 3 caracteres!";
+        errorContainer.appendChild(error);
+        
+        
+
     }
 
     else{
         axios.get(`https://api.github.com/search/users?q=${query}`)
         .then(res => {
-            console.log(res)
             const users = res.data.items;
             for (let i = 0; i < users.length && i < 20; i++){
                 const li = document.createElement('li');
